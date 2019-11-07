e7edddedc0ec92f1c285098b337d6a5c8458f297
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2019-11-07 11:47:55 +0700

    add merge sort

--- src/AppSort.ts
+++ src/AppSort.ts
@@ -1,32 +1,18 @@
 import { BubbleSort } from "./sort/BubbleSort";
 import { InsertionSort } from "./sort/InsertionSort";
-import { SortHelper } from "./sort/SortHelper";
+import { MergeSort } from "./sort/MergeSort";
 
 let unsortedNums: number[] = [3, 4, 5, 2, 1];
 
 function sortNumbers(unsortedNums: number[]) {
     let bubbleSort = new BubbleSort();
     bubbleSort.sort(unsortedNums);
-}
 
-function otherSortNumbers(unsortedNums: number[]) {
     let insertionSort = new InsertionSort();
     insertionSort.sort(unsortedNums);
-}
 
-function swap() {
-    let nums: number[] = [3, 4, 1, 2];
-    let sortHelper = new SortHelper();
-    sortHelper.xorSwap(nums[0], nums[2]);
-
-    console.log(`after xor swap: ${nums[0]} vs ${nums[2]}`);
-
-    [nums[0], nums[2]] = sortHelper.swap(nums[0], nums[2]);
-    console.log(`after destructing swap: ${nums[0]} vs ${nums[2]}`);
+    let mergeSort = new MergeSort();
+    mergeSort.sort(unsortedNums);
 }
 
 sortNumbers(unsortedNums);
-
-otherSortNumbers(unsortedNums);
-
-swap();
---
+++ src/sort/MergeSort.ts
@@ -0,0 +1,9 @@
+import { Sort } from "./Sort";
+
+export class MergeSort implements Sort {
+
+    sort(nums: number[]): number[] {
+        throw new Error("Method not implemented.");
+    }
+
+}
