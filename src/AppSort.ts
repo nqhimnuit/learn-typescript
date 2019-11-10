0ee8a7f0b269bcb4ccf4cdefa0f0dc6ee99ec376
Minh Quoc Nguyen <nqminhuit@gmail.com>
2019-11-10 18:22:58 +0700

    add promise (raw, unclean code)

--- src/AppSort.ts
+++ src/AppSort.ts
@@ -5,51 +5,49 @@ import { Sort } from "./sort/Sort";
 
 const ARRAY_LENGTH = 10000;
 
-const UNSORTED_ARRAY: number[] = initializeArray(ARRAY_LENGTH);
-Object.freeze(UNSORTED_ARRAY);
-
 function getRandomInt(max: number): number {
-    return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 function initializeArray(arrayLength: number): number[] {
-    let nums: number[] = [];
-    for (let i = 0; i < arrayLength; i++) {
-        nums.push(getRandomInt(arrayLength));
-    }
-    console.log('unsorted array = ', nums);
-    return nums;
+  let nums: number[] = [];
+  for (let i = 0; i < arrayLength; i++) {
+    nums.push(getRandomInt(arrayLength));
+  }
+  console.log('unsorted array = ', nums);
+  return nums;
 }
 
 function isSortedArray(nums: number[]): boolean {
-    return nums.every(function (x, i) {
-        return i === 0 || x >= nums[i - 1];
-    });
+  return nums.every(function (x, i) {
+    return i === 0 || x >= nums[i - 1];
+  });
 }
 
-function sortAlgorithm(sortAlgorithm: Sort): void {
-    if (isSortedArray(UNSORTED_ARRAY)) {
-        console.error('!!! array is already sorted wont do anything !!!');
-        return;
-    }
-
-    let start: number = Date.now();
-    let sortedArray = sortAlgorithm.sort(UNSORTED_ARRAY);
-    let timeTaken: number = Date.now() - start;
-
-    let isSorted: boolean = isSortedArray(sortedArray);
-    if (isSorted) {
-        console.log(`--- Sorted with ${sortAlgorithm.sortName}, took ${timeTaken} ms`);
+new Promise(
+  (resolve, reject) => {
+    let unsortedArray = initializeArray(2);
+    if (!isSortedArray(unsortedArray)) {
+      resolve(unsortedArray);
     }
     else {
-        console.error('!!! cannot sort array !!!');
+      let errorMsg = 'array is already sorted, nothing to do';
+      reject(errorMsg);
     }
-}
-
-function sortNumbers() {
-    sortAlgorithm(new MergeSort());
-    sortAlgorithm(new InsertionSort());
-    sortAlgorithm(new BubbleSort());
-}
-
-sortNumbers();
+  })
+  .then(
+    function (nums) {
+      let mergeSort: Sort = new MergeSort();
+      const sortedNums: number[] = mergeSort.sort(<number[]>nums);
+      console.log('sorting is done');
+      if (isSortedArray(sortedNums)) {
+        return sortedNums;
+      }
+      return "there is something wrong with sort algorithm";
+    },
+    function (error) {
+      throw `there is something wrong with sort algorithm: '${error}'`;
+    })
+  .then(
+    (nums) => { console.log('sorting successful: ', nums) })
+  .catch(function (error) { console.log(error) });
